e20fd4cb7f6aef74704c4b3a5ea161674f7a887b
tomasgeneroso <tomasgeneroso90@gmail.com>
2024-10-03 21:48:32 +0200

    .

--- ToDoList/src/App.tsx
+++ ToDoList/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import CardList from "./components/CardList";
+import { Todo } from "../types/type";
 
 function App() {
-  return <></>;
+  const [search, setSearch] = useState<Todo[]>([]);
+
+  return (
+    <>
+      <h1 style={{ marginTop: "2rem" }}>JUST DO IT</h1>
+      <div className="container">
+        <SearchBar setSearch={setSearch} search={search} />
+        <CardList todo={search} setSearch={setSearch} />
+      </div>
+    </>
+  );
 }
 
 export default App;
